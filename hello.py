efb1127fa2cefc9cbe1797db934e13c9ce7929ea
RadhikaHira <RadhikaHira@users.noreply.github.com>
2021-01-05 21:14:59 +0000

    automated commit by submit50

---
+++ hello.py
@@ -0,0 +1,6 @@
+# importing and using get_string from cs50 library 
+from cs50 import get_string
+
+# get name and put in variable name and then print 
+name = get_string("What is your name? \n")
+print("hello , " + name)
